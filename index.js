3bf7625b62f05992be9d8bcefe61e10fde443348
Nivash_sharma <nivasmathu@gmail.com>
2024-01-04 15:28:51 +0530

    insert template

---
+++ index.js
@@ -0,0 +1,16 @@
+document.getElementById('importVideo').addEventListener('click', function() {
+    // Code to import video
+    console.log('Import Video clicked');
+});
+
+document.getElementById('toggleDrawing').addEventListener('click', function() {
+    // Code to toggle drawing
+    console.log('Toggle Drawing clicked');
+});
+
+document.getElementById('togglePan').addEventListener('click', function() {
+    // Code to toggle pan
+    console.log('Toggle Pan clicked');
+});
+
+// Add more event listeners for other buttons
